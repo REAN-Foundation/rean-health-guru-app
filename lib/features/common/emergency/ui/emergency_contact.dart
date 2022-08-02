b79f0650be5e62dba746f1b37e466a5a63ef1efb
Tushar Katakdound <tushar02.katakdound@gmail.com>
2022-08-02 18:23:06 +0530

    Emergency Contact -> gender removed from add doctor dialog

--- lib/features/common/emergency/ui/emergency_contact.dart
+++ lib/features/common/emergency/ui/emergency_contact.dart
@@ -1191,7 +1191,7 @@ class _EmergencyContactViewState extends State<EmergencyContactView> {
         //child: addOrEditAllergiesDialog(context),
         child: Container(
           width: double.infinity,
-          height: 640,
+          height: 540,
           child: Column(
             children: [
               Row(

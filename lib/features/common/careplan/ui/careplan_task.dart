e60a2042bfb6a7b8d70bbbaec3f1fcce642c56cb
Tushar Katakdound <70588110+tushar-tikme-co@users.noreply.github.com>
2022-10-17 17:47:33 +0530

    screen grey out issue fixed (#170)
    Co-authored-by: Tushar Katakdound <tushar02.katakdound@gmail.com>

--- lib/features/common/careplan/ui/careplan_task.dart
+++ lib/features/common/careplan/ui/careplan_task.dart
@@ -1952,13 +1952,17 @@ class _CarePlanTasksViewState extends State<CarePlanTasksView>
               //showToast('Task completed successfully', context);
             });
           } else {
-            Navigator.pushNamed(context, RoutePaths.Assessment_Score_Navigator,
-                arguments: task.action!.assessment!.id)
-                .then((value) {
-              getUserTask();
-              //showToast('Task completed successfully', context);
-            });
-            showToast('Task is already completed', context);
+            if(task.action!.assessment!.title == 'Quality of Life Questionnaire') {
+              Navigator.pushNamed(
+                  context, RoutePaths.Assessment_Score_Navigator,
+                  arguments: task.action!.assessment!.id)
+                  .then((value) {
+                getUserTask();
+                //showToast('Task completed successfully', context);
+              });
+            }else {
+              showToast('Task is already completed', context);
+            }
           }
           //Navigator.pushNamed(context, RoutePaths.Assessment_Start_Care_Plan);
           break;

07a9719efcae084155f1892a1b99d4a7835c18db
Tushar Katakdound <tushar02.katakdound@gmail.com>
2022-11-14 18:28:43 +0530

    Solve accessibilty issue & gender option added

--- lib/features/misc/ui/edit_patient_medical_profile.dart
+++ lib/features/misc/ui/edit_patient_medical_profile.dart
@@ -1555,9 +1555,9 @@ class _EditPatientMedicalProfileViewState
     }
   }
 
-  _fieldFocusChange(
+/*  _fieldFocusChange(
       BuildContext context, FocusNode currentFocus, FocusNode nextFocus) {
     currentFocus.unfocus();
     FocusScope.of(context).requestFocus(nextFocus);
-  }
+  }*/
 }
